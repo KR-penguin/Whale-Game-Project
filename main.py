4544ec63ea8841465c64b3cad8e8a547ef1ec96e
kopeng <kkwonsc@gmail.com>
2024-01-12 15:03:45 +0900

    chore : repl 사용 폐지(3)
    repl에서 일반적인 ide 사용을 하기로 해서 코드를 옮김.
    그 과정에서 불필요한 파일들을 삭제함.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 import os
-import sys
 import threading
 import pygame
 from pygame.locals import QUIT  # Pygame 초기화
@@ -9,9 +8,9 @@ pygame.init()
 
 # --- 화면 설정 ---
 
-Screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-ScreenWidth = Screen.get_width()
-ScreenHeight = Screen.get_height()
+Screen = pygame.display.set_mode((1920, 1080))  # 화면 크기
+ScreenWidth = 1920
+ScreenHeight = 1080
 
 pygame.display.set_caption("Whale")  # 게임 제목 설정
 
@@ -19,24 +18,112 @@ Clock = pygame.time.Clock()  # fps 설정을 위한 Clock 변수 설정
 
 SceneValue = 0  # 장면 값
 
-# --- image load & setup ---
+# --- 이미지 불러오기 ---
 BasicImagePath = os.path.abspath(
   '.') + '/' + "sources/images/"  # image 기본 경로 설정
-TestImage = pygame.image.load(BasicImagePath + "m.png")
+PlayerImage = pygame.image.load(BasicImagePath + "test.png")
 
-TestImageRect = TestImage.get_rect()
-TestImageSize = TestImageRect.size
-TestImageWidth = TestImageSize[0]
-TestImageHeight = TestImageSize[1]
+# --- classes ---
 
-while True:
+
+class Object:
+  Size = 0
+  Width = 0
+  Height = 0
+  Xpos = 0
+  Ypos = 0
+
+
+class HUD:
+  Xpos = 0
+  Ypos = 0
+
+
+class DynamicObject(Object):
+  ToXpos = 0
+  ToYpos = 0
+
+
+class StaticObject(Object):
+  pass
+
+
+class Character(DynamicObject):
+
+  def __init__(self, Image, Xpos, Ypos, Speed):
+    # 상속된 변수들
+    self.Size = Image.get_rect().size
+    self.Width = self.Size[0]
+    self.Height = self.Size[1]
+    self.Xpos = Xpos
+    self.Ypos = Ypos
+    # 이 class에서 새로 선언된 변수들
+    self.ToXpos = 0
+    self.ToYpos = 0
+    self.speed = Speed
+
+
+class Background(DynamicObject):
+
+  def __init__(self, Image, Xpos, Ypos, Speed):
+    # 상속된 변수들
+    self.Size = Image.get_rect().size
+    self.Width = self.Size[0]
+    self.Height = self.Size[1]
+    self.Xpos = Xpos
+    self.Ypos = Ypos
+    # 이 class에서 새로 선언된 변수들
+    self.ToXpos = 0
+    self.ToYpos = 0
+    self.speed = Speed
+
+
+class VirtualJoystick(HUD):
+
+  def __init__(self, Image, Xpos, Ypos):
+    # 상속된 변수들
+    self.Size = Image.get_rect().size
+    self.Width = self.Size[0]
+    self.Height = self.Size[1]
+    self.Xpos = Xpos
+    self.Ypos = Ypos
+
+
+# --- create instance ---
+
+Player = Character(PlayerImage, 0, 0, 0.3)
+
+# --- begin setup ---
+
+Running = True
+
+# --- main loop ---
+
+while Running:
   for event in pygame.event.get():
     if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
+      Running = False
 
-  Screen.blit(TestImage, (ScreenWidth / 2 - TestImageWidth / 2,
-                          ScreenHeight / 2 - TestImageHeight / 2))
+    # --- Key binding ---
+
+    # Key가 눌러졌는가?
+    if event.type == pygame.KEYDOWN:
+
+      # 좌우 Movement 키바인딩
+      if event.key == pygame.K_d:
+        Player.ToXpos += Player.speed
+      elif event.key == pygame.K_a:
+        Player.ToXpos -= Player.speed
+      # 상하 Movement 키바인딩
+      elif event.key == pygame.K_w:
+        Player.ToYpos += Player.speed
+      elif event.key == pygame.K_s:
+        Player.ToYpos -= Player.speed
+
+  # --- draw objects on screen ---
+
+  Screen.fill((255, 255, 255))
+  Screen.blit(PlayerImage, (0, 0))
 
   pygame.display.update()
 
