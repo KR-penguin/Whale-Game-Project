1ed05fee228aed6362ea7ceb32a30d426d084f1f
kopeng <kkwonsc@gmail.com>
2024-01-28 23:55:25 +0900

    feat : Animation Class로 Animation 작업 분할
    Animation Class를 만들어서 Character 클래스와 Background 클래스가 집적 관리하던 Animation 처리부분을 다른 Class가 담당하도록 수정함.

--- game_class.py
+++ game_class.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import typing
 
 # --- classes ---
 class Object:
@@ -30,6 +31,52 @@ class MouseInfo:
 class GameModeBase:
   def __init__(self, FrictionalForce):
     self.FrictionalForce = FrictionalForce
+
+
+
+class BasicAnimation():
+  def __init__(self, MaxFrame):
+    self.Temp = 0
+    self.AnimationFrame = 0
+    self.AnimationTotalFrame = MaxFrame
+  
+  def update_animation(self):
+    self.Temp += 0.1
+    self.AnimationFrame = math.floor(self.Temp)
+
+    if (self.AnimationFrame >= self.AnimationTotalFrame): # Animation Frame이 끝났다면
+      self.AnimationFrame = 0
+      self.Temp = 0
+
+
+
+class HighQualityAnimation():
+  def __init__(self, MaxFrame : typing.List[int]):
+    self.Temp = 0
+    self.Status = "Idle" # Status에는 Idle, Run, Jump 등이 있다.
+    self.AnimationFrame = [0, 0] # 첫번째 index는 status를 나타내고, 두번째 index는 현재 Animation의 frame을 나타냄.
+    self.AnimationTotalFrame = MaxFrame # AnimationTotalFrame은 list로써 첫번째 index는 첫번째 status(idle)의 총 frame 개수를 나타낸다.
+
+  def update_animation(self):
+    self.Temp += 0.1
+
+    if (self.Status == "Idle"):
+      self.AnimationFrame = [0, math.floor(self.Temp)]
+      if(self.AnimationFrame[1] >= self.AnimationTotalFrame[0]): # Animation Frame이 Animation Max Frame의 첫번째 index(Idle 애니메이션의 Max Frame)과 같거나 더 클때 (Animation이 끝났을 때)
+        self.AnimationFrame[1] = 0 # Animation Frame을 0으로 초기화함.
+        self.Temp = 0
+
+    elif (self.Status == "Run"):
+      self.AnimationFrame = [1, math.floor(self.Temp)]
+      if(self.AnimationFrame[1] >= self.AnimationTotalFrame[1]): # Animation Frame이 8과 같거나 더 클때 (Animation이 끝났을 때)
+        self.AnimationFrame[1] = 0 # Animation Frame을 0으로 초기화함.
+        self.Temp = 0
+
+    elif (self.Status == "Jump"):
+      self.AnimationFrame = [2, math.floor(self.Temp)]
+      if(self.AnimationFrame[1] >= self.AnimationTotalFrame[2]): # Animation Frame이 8과 같거나 더 클때 (Animation이 끝났을 때)
+        self.AnimationFrame[1] = 0 # Animation Frame을 0으로 초기화함.
+        self.Temp = 0    
 
 
 
@@ -66,54 +113,23 @@ class StaticObject(Object):
 
 
 
-class Character(DynamicObject):
+class Character(DynamicObject, HighQualityAnimation):
 
   def __init__(self, Image, Xpos, Ypos, Speed):
-    super().__init__(Image, Xpos, Ypos)
+    DynamicObject.__init__(self, Image, Xpos, Ypos)
+    HighQualityAnimation.__init__(self, [8, 8, 4])
     self.Speed = Speed
     self.bMove = False
-    self.Status = "Idle" # Status에는 Idle, Run, Jump가 있다.
-    self.AnimationFrame = [0, 0] # 첫번째 index는 status를 나타내고, 두번째 index는 현재 Animation의 frame을 나타냄.
-    self.Temp = 0
-
-  def update_animation(self):
-    self.Temp += 0.1
-
-    if (self.Status == "Idle"):
-      self.AnimationFrame = [0, math.floor(self.Temp)]
-      if(self.AnimationFrame[1] >= 8): # Animation Frame이 8과 같거나 더 클때 (Animation이 끝났을 때)
-        self.AnimationFrame[1] = 0 # Animation Frame을 0으로 초기화함.
-        self.Temp = 0
-
-    elif (self.Status == "Run"):
-      self.AnimationFrame = [1, math.floor(self.Temp)]
-      if(self.AnimationFrame[1] >= 8): # Animation Frame이 8과 같거나 더 클때 (Animation이 끝났을 때)
-        self.AnimationFrame[1] = 0 # Animation Frame을 0으로 초기화함.
-        self.Temp = 0
-
-    elif (self.Status == "Jump"):
-      self.AnimationFrame = [2, math.floor(self.Temp)]
-      if(self.AnimationFrame[1] >= 4): # Animation Frame이 8과 같거나 더 클때 (Animation이 끝났을 때)
-        self.AnimationFrame[1] = 0 # Animation Frame을 0으로 초기화함.
-        self.Temp = 0
 
 
 
-class Background(DynamicObject):
+class Background(DynamicObject, BasicAnimation):
 
   def __init__(self, Image, Xpos, Ypos):
-    super().__init__(Image, Xpos, Ypos)
+    DynamicObject.__init__(self, Image, Xpos, Ypos)
+    BasicAnimation.__init__(self, 8)
     self.bMove = False
-    self.AnimationFrame = 0 # Background 같은 경우는 Status가 필요하지 않음.
-    self.Temp = 0
 
-  def update_animation(self):
-    self.Temp += 0.1
-    self.AnimationFrame = math.floor(self.Temp)
-
-    if (self.AnimationFrame >= 8): # Animation Frame이 끝났다면
-      self.AnimationFrame = 0
-      self.Temp = 0
 
 
 class Button(HUD, Object):
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import threading
 import pygame
 import game_class
+import typing
 
 pygame.init()
 
